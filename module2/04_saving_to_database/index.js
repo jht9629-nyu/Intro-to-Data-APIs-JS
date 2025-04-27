6970bfeaf03b6aec0351ab96f090476420261095
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-04-27 19:21:35 -0400

    seald-io/nedb

--- module2/04_saving_to_database/index.js
+++ module2/04_saving_to_database/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const Datastore = require('nedb');
+const Datastore = require('@seald-io/nedb');
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-const database = new Datastore('database.db');
+const database = new Datastore({ filename: 'database.db' });
+// const db = new Datastore({ filename: 'path/to/datafile' })
 database.loadDatabase();
 
 app.post('/api', (request, response) => {
